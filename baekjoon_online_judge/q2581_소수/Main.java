ddbec8bb04398b0fc1f7dddef67d054e9cf3297c
jonny-ddns <biggux100@gmail.com>
2021-10-02 08:17:39 +0900

    update code

---
+++ baekjoon_online_judge/q2581_소수/Main.java
@@ -0,0 +1,78 @@
+package baekjoon_online_judge.q2581_소수;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new Solution();
+    }
+
+    static class Solution {
+        private Reader reader;
+        private Writer writer;
+        private BufferedReader bufferedReader;
+        private BufferedWriter bufferedWriter;
+
+        private Solution() {
+            resourceOpen();
+            execute();
+        }
+
+        //입출력
+        private void execute() {
+            try{
+                String M = bufferedReader.readLine().trim();
+                String N = bufferedReader.readLine().trim();
+                bufferedWriter.write( solve(M, N) );
+                bufferedWriter.flush();
+            } catch (IOException io){
+                io.printStackTrace();
+            } finally {
+                try {
+                    resourceClose();
+                } catch (IOException io){
+                    io.printStackTrace();
+                }
+            }
+        }
+
+        //연산
+        private String solve(String x, String y) {
+            int start = Integer.parseInt(x);
+            int end = Integer.parseInt(y);
+            return sum(start, end) + "\n" + min(start, end);
+        }
+        
+        //1. 소수의 합
+        private int sum(int x, int y){
+
+            return 0;
+
+        }
+        
+        
+        
+        //2. 최솟값
+        private int min(int x, int y){
+                     return 0;
+        }
+
+
+        /*---------------------------------------------*/
+        //자원할당
+        private void resourceOpen(){
+            reader = new InputStreamReader(System.in);
+            writer = new OutputStreamWriter(System.out);
+            bufferedReader = new BufferedReader(reader);
+            bufferedWriter = new BufferedWriter(writer);
+        }
+
+        //자원해제
+        private void resourceClose() throws IOException {
+            if (reader != null)             reader.close();
+            if (writer != null)             writer.close();
+            if (bufferedReader != null)     bufferedReader.close();
+            if (bufferedWriter != null)     bufferedWriter.close();
+        }
+    }
+}
