e69f137369b49d47198c7341a154c0ae4d416c0b
root <biggux100@gmail.com>
2021-10-15 15:25:48 +0900

    update code

---
+++ baekjoon_online_judge/q0000_test/Test.java
@@ -0,0 +1,11 @@
+package baekjoon_online_judge.q0000_test;
+
+public class Test {
+    public static void main(String[] args) {
+        String s = new String("print test");
+        new Test().printOut(s);
+    }
+    private void printOut(String s){
+        System.out.println(s);
+    }
+}
